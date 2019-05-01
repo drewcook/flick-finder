f97a3904bafafce44b660fd01823eda4941a2e86
Drew Cook <drewcook29@gmail.com>
2019-05-01 10:39:39 -0600

    server side routing

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const next = require("next");
 const mongoose = require("mongoose");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({dev});
+const handle = app.getRequestHandler();
 
 // middleware
 const bodyParser = require("body-parser");
@@ -7,18 +12,46 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 // allow env vars
-require("dotenv").config({path: "variables.env"});
+//require("dotenv").config({path: "variables.env"});
 
 // init app
-const app = express();
-app.use(cors);
-
 const PORT = process.env.PORT || 5000;
+app
+	.prepare()
+	.then(() => {
+		const server = express();
+		//server.use(cors);
 
-app.listen(PORT, (err) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(`App is listening on port: ${PORT}`);
-	}
-})
+		// routes
+		server.get("/browse", (req, res) => {
+			const actualPage = "/Browse";
+			const queryParams = {};
+			app.render(req, res, actualPage, queryParams);
+		});
+
+		server.get("/signin", (req, res) => {
+			const actualPage = "/SignIn";
+			const queryParams = {};
+			app.render(req, res, actualPage, queryParams);
+		});
+
+		server.get("/signup", (req, res) => {
+			const actualPage = "/SignUp";
+			const queryParams = {};
+			app.render(req, res, actualPage, queryParams);
+		});
+
+		server.get("*", (req, res) => {
+			return handle(req, res);
+		});
+
+		// listen
+		server.listen(PORT, (err) => {
+			if (err) throw err;
+			console.log(`App is listening on port: ${PORT}`);
+		});
+	})
+	.catch(ex => {
+		console.log(ex.stack);
+		process.exit(1);
+	});
