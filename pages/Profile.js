504cc065c220d7ca37b64cfd75c046dfd48b30f0
Drew Cook <drewcook29@gmail.com>
2019-05-01 11:31:03 -0600

    Add profile page

---
+++ pages/Profile.js
@@ -0,0 +1,12 @@
+import AppLayout from "../components/AppLayout";
+
+const Profile = (props) => (
+	<AppLayout>
+		<div>
+			<h2>Profile</h2>
+			<p>Here are some details of the user.</p>
+		</div>
+	</AppLayout>
+);
+
+export default Profile;
--- server.js
+++ server.js
@@ -1,26 +1,52 @@
 const express = require("express");
 const next = require("next");
-const mongoose = require("mongoose");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({dev});
 const handle = app.getRequestHandler();
+
+// models
+const User = require("./models/User");
+
+// graphQL setup
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers
+});
 
 // middleware
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
-// allow env vars
-//require("dotenv").config({path: "variables.env"});
+// connect to db
+const mongoose = require("mongoose");
+require("dotenv").config({path: "variables.env"});
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Mongo DB connected"))
+	.catch(err => console.log(err));
 
 // init app
 const PORT = process.env.PORT || 5000;
 app
 	.prepare()
 	.then(() => {
+		// initial setup
 		const server = express();
 		//server.use(cors);
+		// connect schemas with graphQL
+		server.use("/graphiql", graphiqlExpress({ endpoint: "/graphql" }));
+		server.use("/graphql", bodyParser.json(), graphqlExpress({
+			schema,
+			context: {
+				User
+			}
+		}))
 
 		// routes
 		server.get("/browse", (req, res) => {
@@ -37,6 +63,12 @@ app
 
 		server.get("/signup", (req, res) => {
 			const actualPage = "/SignUp";
+			const queryParams = {};
+			app.render(req, res, actualPage, queryParams);
+		});
+
+		server.get("/profile", (req, res) => {
+			const actualPage = "/Profile";
 			const queryParams = {};
 			app.render(req, res, actualPage, queryParams);
 		});
