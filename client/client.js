ecd73c6a37f20b6038d4f3873511e50ccb6904b1
drewcook <drewcook29@gmail.com>
2021-05-26 21:49:07 -0600

    setContext for gql requests

--- client/client.js
+++ client/client.js
@@ -5,22 +5,24 @@ const fetch = require("isomorphic-unfetch");
 const dev = process.env.NODE_ENV !== "production";
 
 const client = new ApolloClient({
-	uri: dev ? "http://localhost:3000/graphql" : "https://flickfinder.herokuapp.com/graphql",
-	fetchOptions: {
-		credentials: "include"
-	},
-	request: operation => {
+	uri: dev
+		? "http://localhost:3000/graphql"
+		: "https://flickfinder.herokuapp.com/graphql",
+	request: (operation) => {
 		// pass along token with every http request to server for authentication
 		const token = localStorage.getItem("userToken");
 		operation.setContext({
+			fetchOptions: {
+				credentials: "include",
+			},
 			headers: {
-				authorization: token ? token : null
-			}
-		})
+				authorization: token ? token : null,
+			},
+		});
 	},
 	onError: ({ networkError }) => {
 		if (networkError) console.log("Network Error -", networkError);
-	}
+	},
 });
 
 exports.client = client;
