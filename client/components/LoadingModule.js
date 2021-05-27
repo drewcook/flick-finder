d375d5a68334772afc75196d687d946cb1c566fa
drewcook <drewcook29@gmail.com>
2021-05-27 11:33:27 -0600

    rename static/ to public/

--- client/components/LoadingModule.js
+++ client/components/LoadingModule.js
@@ -1,13 +1,15 @@
-const LoadingModule = props => (
+const LoadingModule = (props) => (
 	<div>
-		<img src="/static/img/loading.gif" width={400} height={400} />
+		<img src="/img/loading.gif" width={400} height={400} />
 		<style jsx>{`
 			div {
 				display: flex;
 				justify-content: center;
 				align-items: center;
 				width: ${props.style && props.style.width ? props.style.width : "100%"};
-				height: ${props.style && props.style.height ? props.style.height : "100%"};
+				height: ${props.style && props.style.height
+					? props.style.height
+					: "100%"};
 			}
 			h4 {
 				font-weight: normal;
