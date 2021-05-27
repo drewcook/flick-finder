1a663d8197d2ceb66f71a1d33bd0047c5af4950a
drewcook <drewcook29@gmail.com>
2021-05-27 11:34:17 -0600

    public refs

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,25 +1,28 @@
 import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { NotificationContainer } from 'react-notifications';
+import { NotificationContainer } from "react-notifications";
 
-const Layout = props => (
+const Layout = (props) => (
 	<div>
 		<Head>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta charSet="utf-8" />
 			<title>Flick Finder | {props.title}</title>
-			<link rel="stylesheet" href="/static/css/bootstrap.min.css" />
-			<link rel="stylesheet" href="/static/css/notifications.min.css" />
-			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
-			<link rel="icon" href="/static/img/favicon.ico" type="image/x-icon" />
+			<link rel="stylesheet" href="/css/bootstrap.min.css" />
+			<link rel="stylesheet" href="/css/notifications.min.css" />
+			<link
+				rel="stylesheet"
+				href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+				integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+				crossOrigin="anonymous"
+			/>
+			<link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
 		</Head>
 		<header>
 			<Navbar session={props.session} />
 		</header>
-		<div className="container">
-			{props.children}
-		</div>
+		<div className="container">{props.children}</div>
 		<Footer />
 		<NotificationContainer />
 		<style jsx global>{`
