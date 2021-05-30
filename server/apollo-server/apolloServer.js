dfbd15da2be6b563e633aaae9792dc9d0fc3c08f
drewcook <drewcook29@gmail.com>
2021-05-30 12:48:19 -0600

    set current user in apollo context

--- server/apollo-server/apolloServer.js
+++ server/apollo-server/apolloServer.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
 const resolvers = require("./resolvers");
 const typeDefs = require("./typeDefs");
+const jwt = require("jsonwebtoken");
 
 // models
 const User = require("../models/User");
@@ -10,14 +11,28 @@ const Movie = require("../models/Movie");
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	context: ({ req }) => ({
-		User,
-		Movie,
-		currentUser: req.currentUser,
-		token: req.headers.authorization || "",
-	}),
+	context: async ({ req }) => {
+		const token = req.headers.authorization?.split(" ")[1];
+
+		// // get current user based off token
+		let currentUser;
+		if (token !== undefined && token !== null) {
+			currentUser = await jwt.verify(token, process.env.USER_SECRET);
+		}
+		return {
+			User,
+			Movie,
+			currentUser,
+			token,
+		};
+	},
 	engine: {
 		apiKey: process.env.ENGINE_API_KEY,
+	},
+	playground: {
+		settings: {
+			"schema.polling.enable": false,
+		},
 	},
 });
 
