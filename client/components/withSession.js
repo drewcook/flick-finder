ed87b41c1e4006b008d20931419b6c70f20519a3
drewcook <drewcook29@gmail.com>
2021-05-30 12:47:30 -0600

    with session

--- client/components/withSession.js
+++ client/components/withSession.js
@@ -3,12 +3,13 @@ import { GET_CURRENT_USER } from "../../queries";
 import LoadingModule from "./LoadingModule";
 
 const withSession = (Component) => (props) => {
-	const { data, loading, refetch } = useQuery(GET_CURRENT_USER);
+	const { data, error, refetch } = useQuery(GET_CURRENT_USER);
 
-	if (loading)
-		return <LoadingModule style={{ width: "100vw", height: "100vh" }} />;
+	if (error) return <p>Uh Oh, an error occurred.</p>;
 
-	return <Component {...props} refetch={refetch} session={data} />;
+	if (data) return <Component {...props} refetch={refetch} session={data} />;
+
+	return <LoadingModule style={{ width: "100vw", height: "100vh" }} />;
 };
 
 export default withSession;
